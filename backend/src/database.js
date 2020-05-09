84dcdd3602485477045004ae065b8391f55692b3
Agustin Fiori <afiori@clarika.com.ar>
2020-05-09 20:29:16 -0300

    mongodb connection

---
+++ backend/src/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const URI = process.env.MONGODB_URI;
+//console.log(URI);
+
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('DB connected!');
+});
