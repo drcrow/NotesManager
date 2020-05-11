ff5b8e9ee8e2fd7e19e2e2ed08defb603c2aa1e6
Agustin Fiori <afiori@clarika.com.ar>
2020-05-11 09:50:20 -0300

    React empty components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import CreateNote from './components/CreateNote';
+import CreateUser from './components/CreateUser';
+import Navigation from './components/Navigation';
+import NotesList from './components/NotesList';
+
 function App() {
   return (
-    <div>Hello World</div>
+    <div>
+      <Navigation/>
+      Hello World
+      </div>
   );
 }
 
